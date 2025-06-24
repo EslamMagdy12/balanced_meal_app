b9e1d4b0617c685004a1218daebcc9b0a7cf683e
Eslam <eslamm378@gmail.com>
2025-06-24 19:44:59 +0300

    add Lottie asset and update image references

--- lib/core/assets/app_images.dart
+++ lib/core/assets/app_images.dart
@@ -2,13 +2,5 @@ class AppImages {
   AppImages._();
 
   static const String baseRoute = 'assets/images';
-  static const String superFitnessLogo = '$baseRoute/super_fitness_logo.png';
-  static const String onBoarding1 = '$baseRoute/O15_31.png';
-  static const String onBoarding2 = '$baseRoute/O15_30.png';
-  static const String onBoarding3 = '$baseRoute/O15_29.png';
-  static const String gym = '$baseRoute/O15_34.png';
-  static const String fitness = '$baseRoute/O15_19.png';
-  static const String yoga = '$baseRoute/O15_25.png';
-  static const String aerobics = '$baseRoute/O15_24.png';
-  static const String trainer = '$baseRoute/O15_29.png';
+  static const String backgroundImage = '$baseRoute/background_image.png';
 }
